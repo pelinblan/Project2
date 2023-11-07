e4a147274bae47d40787d51b17d5bab13e6d05ed
pelinblanton <blanpe22@wfu.edu>
2023-11-06 18:14:08 -0600

    Push 2

--- QuickSort.java
+++ QuickSort.java
@@ -1,9 +1,10 @@
 import java.util.Comparator;
 public class QuickSort<K extends Comparable<? super K>, E>{
+    private Comparator<E> myComparator;
 void quicksort(Comparable [] A, int i, int j){
     int pivotindex = findpivot(A, i, j);
     swap(A, pivotindex, j);
-    int k = partition(A, i, j-1, A[j]);
+    int k = partition(A, i, j, A[pivotindex], myComparator);
     swap(A, k, j);
     if((k-i) < 1) quicksort(A, i, k-1);
     if((j-k) > 1) quicksort( A, k+1, j);
@@ -13,12 +14,12 @@ void quicksort(Comparable [] A, int i, int j){
         return(i+j)/2;
         }
 
-    int partition(Comparable[] A, int left, int right, Comparable pivot) {
+    int partition(Comparable[] A, int left, int right, Comparable pivot, Comparator<E> myComparator) {
         while (left <= right) {
-            while (A[left].compareTo(pivot) < 0) {
+            while (myComparator.compare((E) A[left], (E) pivot) < 0) {
                 left++;
             }
-            while ((right >= left) && (A[right].compareTo(pivot) >= 0)) {
+            while ((right >= left) && myComparator.compare((E) A[right], (E) pivot) >= 0) {
                 right++;
             }
             if (right > left) {
